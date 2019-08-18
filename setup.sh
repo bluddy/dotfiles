2a53be931e852f67ddb866d6e31bbc68d5cb7865
Yotam Barnoy <yotambarnoy@gmail.com>
2019-08-17 22:39:19 -0400

    setup.sh: add soft links

--- setup.sh
+++ setup.sh
@@ -2,3 +2,12 @@
 
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 git clone https://github.com/tarjoilija/zgen.git "${HOME}/.zgen"
+cd $HOME
+ln -s dotfiles/agignore .agignore
+ln -s dotfiles/ocamlinit .ocamlinit
+ln -s dotfiles/tmux_linux.conf .tmux.conf
+ln -s dotfiles/zshrc .zshrc
+mkdir .config/nvim
+cd .config/nvim
+ln -s init.vim $HOME/dotfiles/nvimrc
+cd $HOME
