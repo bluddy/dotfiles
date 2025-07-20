0a42bbe3d17db9649d7c00eca63e7b942fdfa89a
Yotam Barnoy <yotam.barnoy@imagry.co>
2025-07-20 08:48:47 +0300

    Switch to lazy
    fixed wiki
    
    add lazy lock

--- nvim2/lua/user/plugins.lua
+++ nvim2/lua/user/plugins.lua
@@ -1,155 +1,276 @@
-local fn = vim.fn
-
--- Automatically install packer
-local install_path = fn.stdpath "data" .. "/site/pack/packer/start/packer.nvim"
-if fn.empty(fn.glob(install_path)) > 0 then
-  PACKER_BOOTSTRAP = fn.system {
+-- Bootstrap lazy.nvim
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
     "git",
     "clone",
-    "--depth",
-    "1",
-    "https://github.com/wbthomason/packer.nvim",
-    install_path,
-  }
-  print "Installing packer close and reopen Neovim..."
-  vim.cmd [[packadd packer.nvim]]
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
 end
+vim.opt.rtp:prepend(lazypath)
 
--- Autocommand that reloads neovim whenever you save the plugins.lua file
-vim.cmd [[
-  augroup packer_user_config
-    autocmd!
-    autocmd BufWritePost plugins.lua source <afile> | PackerSync
-  augroup end
-]]
+-- Configure lazy.nvim
+require("lazy").setup({
+  -- Core dependencies
+  { "nvim-lua/plenary.nvim" }, -- Useful lua functions used by lots of plugins
 
--- Use a protected call so we don't error out on first use
-local status_ok, packer = pcall(require, "packer")
-if not status_ok then
-  return
-end
-
--- Have packer use a popup window
-packer.init {
-  display = {
-    open_fn = function()
-      return require("packer.util").float { border = "rounded" }
+  -- Autopairs
+  { 
+    "windwp/nvim-autopairs",
+    event = "InsertEnter",
+    config = function()
+      require("user.plugin_config.autopairs")
     end,
   },
-  git = {
-    clone_timeout = 300, -- Timeout, in seconds, for git clones
-  },
-}
 
--- Install your plugins here
-return packer.startup(function(use)
-  -- My plugins here
-  use { "wbthomason/packer.nvim"} -- Have packer manage itself
-  use { "nvim-lua/plenary.nvim"} -- Useful lua functions used by lots of plugins
-  use { "windwp/nvim-autopairs"} -- Autopairs, integrates with both cmp and treesitter
-  use { "numToStr/Comment.nvim"}
-  use { "JoosepAlviste/nvim-ts-context-commentstring", commit = "32d9627123321db65a4f158b72b757bcaef1a3f4" }
-  use { "kyazdani42/nvim-web-devicons", commit = "563f3635c2d8a7be7933b9e547f7c178ba0d4352" }
-  -- use { "kyazdani42/nvim-tree.lua"}
-  use { "akinsho/bufferline.nvim"}
-  use { "moll/vim-bbye"}
-  use { "nvim-lualine/lualine.nvim"}
-  use { "akinsho/toggleterm.nvim"}
-  use { "ahmedkhalf/project.nvim"}
-  use { "lewis6991/impatient.nvim"}
-  use { "goolord/alpha-nvim"}
-  use ({ "kylechui/nvim-surround",
-        config = function()
-          require("nvim-surround").setup({})
-        end
-  })
+  -- Comments
+  { 
+    "numToStr/Comment.nvim",
+    event = { "BufReadPre", "BufNewFile" },
+    config = function()
+      require("user.plugin_config.comment")
+    end,
+  },
+  { "JoosepAlviste/nvim-ts-context-commentstring" },
+
+  -- UI Components
+  { "kyazdani42/nvim-web-devicons" },
+
+  -- Bufferline
+  { 
+    "akinsho/bufferline.nvim",
+    event = "VeryLazy",
+    dependencies = { "kyazdani42/nvim-web-devicons" },
+    config = function()
+      require("user.plugin_config.bufferline")
+    end,
+  },
+  { "moll/vim-bbye" },
+
+  -- Statusline
+  { 
+    "nvim-lualine/lualine.nvim",
+    event = "VeryLazy",
+    config = function()
+      require("user.plugin_config.lualine")
+    end,
+  },
+
+  -- Terminal
+  { 
+    "akinsho/toggleterm.nvim",
+    cmd = "ToggleTerm",
+    config = function()
+      require("user.plugin_config.toggleterm")
+    end,
+  },
+
+  -- Project management
+  { 
+    "ahmedkhalf/project.nvim",
+    config = function()
+      require("user.plugin_config.project")
+    end,
+  },
+
+  -- Performance
+  { 
+    "lewis6991/impatient.nvim",
+    config = function()
+      require("user.plugin_config.impatient")
+    end,
+  },
+
+  -- Dashboard
+  { 
+    "goolord/alpha-nvim",
+    event = "VimEnter",
+    config = function()
+      require("user.plugin_config.alpha")
+    end,
+  },
+
+  -- Surround
+  { 
+    "kylechui/nvim-surround",
+    version = "*", -- Use for stability; omit to use `main` branch for the latest features
+    event = "VeryLazy",
+    config = function()
+      require("nvim-surround").setup({})
+    end
+  },
 
   -- Colorschemes
-  use { "ishan9299/nvim-solarized-lua"}
+  { 
+    "ishan9299/nvim-solarized-lua",
+    priority = 1000,
+    config = function()
+      require("user.plugin_config.colorscheme")
+    end,
+  },
 
-  -- cmp plugins
-  use { "hrsh7th/nvim-cmp"} -- The completion plugin
-  use { "hrsh7th/cmp-buffer"} -- buffer completions
-  use { "hrsh7th/cmp-path"} -- path completions
-  use { "saadparwaiz1/cmp_luasnip"} -- snippet completions
-  use { "hrsh7th/cmp-nvim-lsp"}
-  use { "hrsh7th/cmp-nvim-lua"}
-
-  -- snippets
-  use { "L3MON4D3/LuaSnip"} --snippet engine
-  use { "rafamadriz/friendly-snippets"} -- a bunch of snippets to use
+  -- Completion
+  { 
+    "hrsh7th/nvim-cmp",
+    event = "InsertEnter",
+    dependencies = {
+      "hrsh7th/cmp-buffer",   -- buffer completions
+      "hrsh7th/cmp-path",     -- path completions
+      "saadparwaiz1/cmp_luasnip", -- snippet completions
+      "hrsh7th/cmp-nvim-lsp",
+      "hrsh7th/cmp-nvim-lua",
+      "L3MON4D3/LuaSnip",     -- snippet engine
+      "rafamadriz/friendly-snippets", -- a bunch of snippets to use
+    },
+    config = function()
+      require("user.plugin_config.cmp")
+    end,
+  },
 
   -- LSP
-  -- use { "williamboman/nvim-lsp-installer", commit = "e9f13d7acaa60aff91c58b923002228668c8c9e6" } -- simple to use language server installer
-  use { "neovim/nvim-lspconfig"} -- enable LSP
-  use { "williamboman/mason.nvim" }
-  use { "williamboman/mason-lspconfig.nvim" }
-  -- use { "jose-elias-alvarez/null-ls.nvim" } -- for formatters and linters
-  -- use { "RRethy/vim-illuminate", commit = "a2e8476af3f3e993bb0d6477438aad3096512e42" }
+  { 
+    "neovim/nvim-lspconfig",
+    event = { "BufReadPre", "BufNewFile" },
+    dependencies = {
+      "mason-org/mason.nvim",
+      "mason-org/mason-lspconfig.nvim",
+    },
+    config = function()
+      require("user.lsp")
+    end,
+  },
+  { 
+    "RRethy/vim-illuminate",
+    event = { "BufReadPost", "BufNewFile" },
+    config = function()
+      require("user.plugin_config.illuminate")
+    end,
+  },
 
   -- Telescope
-  use { "nvim-telescope/telescope.nvim"}
+  { 
+    "nvim-telescope/telescope.nvim",
+    cmd = "Telescope",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    config = function()
+      require("user.plugin_config.telescope")
+    end,
+  },
 
   -- Treesitter
-  use {
-    "nvim-treesitter/nvim-treesitter" }
+  { 
+    "nvim-treesitter/nvim-treesitter",
+    event = { "BufReadPost", "BufNewFile" },
+    build = ":TSUpdate",
+    config = function()
+      require("user.plugin_config.treesitter")
+    end,
+  },
+
+  -- Indent guides
+  {
+    "lukas-reineke/indent-blankline.nvim",
+    event = { "BufReadPost", "BufNewFile" },
+    main = "ibl",
+    config = function()
+      require("user.plugin_config.indentline")
+    end,
+  },
 
   -- Git
-  use { "lewis6991/gitsigns.nvim"}
-  use { "jreybert/vimagit" }
-
-  -- DA
-  use { "mfussenegger/nvim-dap", commit = "6b12294a57001d994022df8acbe2ef7327d30587" }
-  use { "rcarriga/nvim-dap-ui", commit = "1cd4764221c91686dcf4d6b62d7a7b2d112e0b13" }
-  use { "ravenxrz/DAPInstall.nvim", commit = "8798b4c36d33723e7bba6ed6e2c202f84bb300de" }
-  -- Lua
-  use {
-    "folke/which-key.nvim",
+  { 
+    "lewis6991/gitsigns.nvim",
+    event = { "BufReadPre", "BufNewFile" },
     config = function()
+      require("user.plugin_config.gitsigns")
+    end,
+  },
+  { "jreybert/vimagit", cmd = "Magit" },
+
+  -- Debug Adapter Protocol
+  { 
+    "mfussenegger/nvim-dap",
+    dependencies = {
+      "rcarriga/nvim-dap-ui",
+      "ravenxrz/DAPInstall.nvim",
+    },
+    config = function()
+      require("user.plugin_config.dap")
+    end,
+  },
+
+  -- Which Key
+  { 
+    "folke/which-key.nvim",
+    event = "VeryLazy",
+    init = function()
       vim.o.timeout = true
       vim.o.timeoutlen = 300
+    end,
+    config = function()
       require("which-key").setup {
         -- your configuration comes here
         -- or leave it empty to use the default settings
         -- refer to the configuration section below
-     }
+      }
     end
-  }
+  },
 
-  -- Oil
-  use({
-      "stevearc/oil.nvim",
-      config = function()
-        require("oil").setup()
-      end,
-    })
-
-  use { "vimwiki/vimwiki" }
-
-  use { "sindrets/diffview.nvim" }
-
-  use { "NeogitOrg/neogit" }
-
-  use ({
-    "epwalsh/obsidian.nvim",
-    tag = "*", -- use latest release
-    requires = {
-      "nvim-lua/plenary.nvim",
-    },
-    })
-
-  use ({ "ggandor/leap.nvim",
-    requires = {
-      "tpope/vim-repeat"
-    },
+  -- Oil file manager
+  {
+    "stevearc/oil.nvim",
+    dependencies = { "kyazdani42/nvim-web-devicons" },
     config = function()
-      require('leap').set_default_mappings()
+      require("oil").setup()
     end,
-  })
+  },
 
-  -- Automatically set up your configuration after cloning packer.nvim
-  -- Put this at the end after all plugins
-  if PACKER_BOOTSTRAP then
-    require("packer").sync()
-  end
-end)
+  -- VimWiki
+  { 
+    "vimwiki/vimwiki",
+    event = "VeryLazy",
+    init = function()
+      -- This runs before the plugin is loaded
+      vim.g.vimwiki_list = {{path='~/wiki/'}}
+    end,
+  },
+
+  -- Git tools
+  { 
+    "sindrets/diffview.nvim",
+    cmd = { "DiffviewOpen", "DiffviewClose", "DiffviewToggleFiles", "DiffviewFocusFiles" },
+    dependencies = { "nvim-lua/plenary.nvim" },
+  },
+
+  { 
+    "NeogitOrg/neogit",
+    dependencies = {
+      "nvim-lua/plenary.nvim",         -- required
+      "sindrets/diffview.nvim",        -- optional - Diff integration
+      "nvim-telescope/telescope.nvim", -- optional
+    },
+    config = true
+  },
+}, {
+  -- Lazy.nvim configuration options
+  ui = {
+    border = "rounded",
+  },
+  performance = {
+    rtp = {
+      disabled_plugins = {
+        "gzip",
+        "tarPlugin",
+        "tohtml",
+        "tutor",
+        "zipPlugin",
+      },
+    },
+  },
+})
+
+-- LSP configuration that was inline in the original file
+require'lspconfig'.ocamllsp.setup{}
