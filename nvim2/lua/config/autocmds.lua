3803f61f88e868c84fde14c35e539f13103dc489
Yotam Barnoy <yotam.barnoy@imagry.co>
2025-09-10 17:57:47 +0300

    Remove spell from markdown

--- nvim2/lua/config/autocmds.lua
+++ nvim2/lua/config/autocmds.lua
@@ -12,7 +12,6 @@ vim.api.nvim_create_autocmd({ "FileType" }, {
   pattern = { "gitcommit", "markdown" },
   callback = function()
     vim.opt_local.wrap = true
-    vim.opt_local.spell = true
   end,
 })
 
