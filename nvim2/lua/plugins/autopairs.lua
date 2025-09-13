d8a55a3f73c3d8189856cacbe5e5ae9af5f1c275
Yotam Barnoy <yotambarnoy@gmail.com>
2025-09-14 00:20:56 +0300

    Disable quote for ocaml

--- nvim2/lua/plugins/autopairs.lua
+++ nvim2/lua/plugins/autopairs.lua
@@ -1,4 +1,12 @@
 return {
   "windwp/nvim-autopairs",
   event = "InsertEnter",
+  config = function()
+    local npairs = require "nvim-autopairs"
+    local Rule = require "nvim-autopairs.rule"
+
+    npairs.setup {}
+
+    require("nvim-autopairs").get_rules("`")[1].not_filetypes = { "ocaml" }
+  end,
 }
