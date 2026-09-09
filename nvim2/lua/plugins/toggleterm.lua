1766457f99f2c2949eb2445709fddf1cc0e0f0d1
Yotam Barnoy <ybarnoy@aligntech.com>
2026-09-09 15:07:44 +0300

    Toggleterm send to terminal keys

--- nvim2/lua/plugins/toggleterm.lua
+++ nvim2/lua/plugins/toggleterm.lua
@@ -1,12 +1,22 @@
 return {
   "akinsho/toggleterm.nvim",
-  cmd = { "ToggleTerm", "TermExec" },
+  cmd = {
+    "ToggleTerm",
+    "TermExec",
+    "ToggleTermSendCurrentLine",
+    "ToggleTermSendVisualSelection",
+    "ToggleTermSendVisualLines",
+  },
   keys = {
     { [[<C-\>]], "<cmd>ToggleTerm<cr>", desc = "Toggle terminal" },
     { "<leader>tt", "<cmd>ToggleTerm<cr>", desc = "Toggle terminal" },
     { "<leader>tf", "<cmd>ToggleTerm direction=float<cr>", desc = "Toggle floating terminal" },
     { "<leader>th", "<cmd>ToggleTerm direction=horizontal<cr>", desc = "Toggle horizontal terminal" },
     { "<leader>tv", "<cmd>ToggleTerm direction=vertical size=80<cr>", desc = "Toggle vertical terminal" },
+    -- Send text to the terminal.
+    { "<leader>ts", "<cmd>ToggleTermSendCurrentLine<cr>", mode = "n", desc = "Send current line to terminal" },
+    { "<leader>ts", "<cmd>ToggleTermSendVisualSelection<cr>", mode = "x", desc = "Send selection to terminal" },
+    { "<leader>tS", "<cmd>ToggleTermSendVisualLines<cr>", mode = "x", desc = "Send selected lines to terminal" },
   },
   opts = {
     -- <C-\> also toggles from inside the terminal.
